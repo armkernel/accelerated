791ab6160ffe8c2dcaaf364b0b4e6b06262c355d
armkernel <sksioi1234@gmail.com>
2019-05-31 00:39:12 +0900

    0531 commit

--- chapter5/combination.cpp
+++ chapter5/combination.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <algorithm>
 
 void read (std::vector<std::string>& vc) {
 	
@@ -12,17 +12,48 @@ void read (std::vector<std::string>& vc) {
 	}
 }
 
+// const & 변화 없이 메모리 낭비 줄이겠다는 의미 
+std::string::size_type width (const std::vector<std::string>& str_vc) {
+	
+	std::string::size_type maxlen = 0;
+
+	for (std::vector<std::string>::size_type i = 0 ; i != str_vc.size(); i++ ){
+		maxlen = std::max(maxlen, str_vc[i].size());
+	}
+
+	return maxlen;
+}
+
+std::vector<std::string> frame (const std::vector<std::string>& str_vc) {
+	
+	std::vector<std::string> tmp_vec;
+
+	std::string::size_type maxlen = width(str_vc); 
+	std::string border(maxlen + 4, '*'); // 4 는 공백 * 이다. 
+
+	tmp_vec.push_back(border);
+	
+	for (std::vector<std::string>::size_type i = 0; i != str_vc.size(); i++) {
+		tmp_vec.push_back("* " + str_vc[i] + std::string(maxlen - str_vc[i].size(), ' ') + " *"); 	
+	}
+
+	tmp_vec.push_back(border);
+
+	return tmp_vec;
+
+}
 
 int main () {
 
-	std::vector<std::string> vc;	
+	std::vector<std::string> vc,ret;	
 	read(vc);
 
 	typedef std::vector<std::string>::size_type vc_sz;
 	vc_sz i = 0;
 
-	for (; i != vc.size(); i++) {
-		std::cout << vc[i] << std::endl;
+	ret = frame(vc);
+	for (; i != ret.size(); i++) {
+		std::cout << ret[i] << std::endl;
 	}
 }
 
