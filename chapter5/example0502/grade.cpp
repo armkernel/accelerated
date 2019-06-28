7224feb8aa1d4bcd48321364c59f24ba2b33c421
armkernel <sksioi1234@gmail.com>
2019-06-28 17:31:23 +0900

    .

--- chapter5/example0502/grade.cpp
+++ chapter5/example0502/grade.cpp
@@ -45,6 +45,7 @@ vector<Student_info> extract_fails(vector<Student_info>& student)
    typedef vector<Student_info>::size_type vec_sz;
 
    // 불변성 : students 백터의 [0, i) 범위에 있는 요소들은 과목을 통과한 학생들의 정보.
+   /*
    vec_sz i = 0; 
    while (i != student.size()) {
        if (fgrade(student[i])) {
@@ -57,9 +58,34 @@ vector<Student_info> extract_fails(vector<Student_info>& student)
        } 
        else
           ++i; 
+   }*/
 
-   }
+  
+   vector<Student_info>::iterator iter = student.begin(); // 만약 iterator가 +연산을 지원하지 않는다면 erase에서 오류남. 
+   // 컨테이너의 반복자를 사용하면, 내가 순차적으로 접근할 수 있지만 임의로 접근하는 것을 막을 수 있다.
+   // 즉, +연산을 막아버리면? 구현을 안하면 접근을 할 수없다.
+   // 다만, index접근을 하게 된다면 문제가됨.
    
+   while (iter != student.end()) 
+   {
+       if(fgrade(*iter)) {
+           fail.push_back(*iter);
+           iter = student.erase(iter);
+
+           // 이 부분이 중요하다. 
+           // iter 변수에 erase를 지우고 반환하는 건 무슨 의미일까? 
+           // 반복자가 제거하고 나면 제거된 이후의 요소가 다 당겨짐. 
+           // 그렇다면 반복자가 무효화 됨. 다시 돌아야지?
+           // 어떻게 기억해? 
+           // 참조 되던 놈이 사라지니까.. 이걸 찾을 방법이 없는데. 반환값으로 이후 참조되는 놈을 반환함.
+           // 그래서 else문에서 증가시켜가면서 찾을 수 있음
+       }
+       else {
+           iter++;
+       }
+   }  
+
+
    return fail;
 }
 
