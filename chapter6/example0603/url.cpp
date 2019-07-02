8a695cab9f9d076b44c057bbc4726d254d071e22
armkernel <sksioi1234@gmail.com>
2019-07-02 22:11:23 +0900

    .

--- chapter6/example0603/url.cpp
+++ chapter6/example0603/url.cpp
@@ -7,6 +7,17 @@
 
 using namespace std;
 
+bool not_url_char(char c)
+{
+    //url에 들어갈 수 있는 알파벳과 숫자 이외의 문자
+    static const string url_ch = "~;/?:@=&$-_.+!*'(),";
+
+    //c가 url에 들어갈 수 있는 문자인지 확인하면 음수를 반환 
+    return !(isalnum(c) || find(url_ch.begin(), url_ch.end(), c) != url_ch.end());
+    // find_if와 다른 find함수는 서술함수를 호출하는 것이 아닌 특정 값을 찾는 함수이다.
+    
+}
+
 string::const_iterator url_end(string::const_iterator b, string::const_iterator e)
 {
     return find_if(b, e, not_url_char);
@@ -15,13 +26,35 @@ string::const_iterator url_end(string::const_iterator b, string::const_iterator 
     // :// 아니면 false를 반환하도록함 
 }
 
-bool not_url_char(char c)
-{
-    //url에 들어갈 수 있는 알파벳과 숫자 이외의 문자
-    static const string url_ch = "~;/?:@=&$-_.+!*'(),";
 
-    //c가 url에 들어갈 수 있는 문자인지 확인하면 음수를 반환 
-    return !(isalnum(c) || find(url_ch.begin(), url_ch.end(), c) != url_ch.end());
+// url_beg 함수가 복잡한 이유 
+// url의 문자인 ://를 포함하지만 url이 아닌 경우가 존재한다. 
+// url 프로토콜의 앞뒤에 유효한 문자가 하나이상 포함될경우 url로 인식하도록 한다. 
+
+string::const_iterator url_beg(string::const_iterator b, string::const_iterator e) 
+{
+    static const string sep = "://"; // 구분자 (delimiter)
+    typedef string::const_iterator iter;
+
+    // i는 구분기호를 발견한 위치를 표시 
+    
+    iter i = b;
+    while ( (i = search(i, e, sep.begin(), sep.end())) != e ) {
+        // ://를 찾을 경우 여기로 들어옴 
+        if (i != b && i + sep.size() != e) {
+            iter beg = i;
+
+            while (beg != b && isalpha(beg[-1]))
+                --beg;
+
+            if (beg != i && !not_url_char(i[sep.size()]))
+                return beg;
+        }
+
+        i += sep.size();
+    }
+
+    return e;
 }
 
 
@@ -52,4 +85,15 @@ vector<string> find_urls(const string& s)
     }
 
     return ret;
+}
+
+int main(){
+
+    string tmp = "https://j";
+    vector<string> ret = find_urls(tmp);
+
+    for(vector<string>::size_type i = 0; i != ret.size(); i++) {
+        cout << ret[i] << endl;
+    }
+    
 }
